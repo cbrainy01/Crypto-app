f508e6685c9dc329ab03b81cb27a7f85f4b2e579
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-02-14 11:33:17 -0600

    update folder structure and fix data handling for api data

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   }
 
   handleCurrencyChange = (newCurrency) => {
-    this.setState({currency: newCurrency})
+    this.setState({ currency: newCurrency })
   }
 
   render() { 
---
+++ src/components/NavbarGlobal/NavbarGlobal.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyledNavbarGlobal } from './NavbarGlobal.styles'
+import { formatNumber } from 'utils/formatNumber'
+
+export class NavbarGlobal extends React.Component {
+  
+
+    render() {
+    const currency = this.props.currency
+    let data = this.props.globalData
+    return( 
+        this.props.globalData && 
+        <StyledNavbarGlobal>
+            <h2>Navbar global</h2>
+            <div>active currencies: {data.active_cryptocurrencies}</div>
+            <div>markets: {data.markets}</div>
+            <div>total market cap: {formatNumber(data.total_market_cap[currency])}</div>
+            <div>total volume: {formatNumber(data.total_volume[currency])}</div>
+            <div>market cap % bitcoin: {Math.round(data.market_cap_percentage["btc"])}%</div>
+            <div>market cap % ethereum: {Math.round(data.market_cap_percentage["eth"])}%</div>
+        </StyledNavbarGlobal>
+    )}
+}
+
+export default NavbarGlobal
