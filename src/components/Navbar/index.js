786d5a8b1c87ed1d34201d747995b300fa8f2ee6
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-02-14 00:30:28 -0600

    add section of navbar for global coin data

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,55 @@
 import React from "react";
+import axios from "axios";
+import NavbarGlobal from "components/NavbarGlobal";
 import { StyledNavbar, StyledLink } from "./Navbar.styles";
 
 export default class Navbar extends React.Component {
+    
+    state = {
+        isLoading: false,
+        error: null,
+        globalData: null,
+    }
+
+    getGlobalCoinsData = async () => {
+        this.setState({ isLoading: true })
+        const response = await axios("https://api.coingecko.com/api/v3/global")
+        this.setState({
+            isLoading: false,
+            globalData: response.data
+        })
+    }
+
+    componentDidMount() {
+        try {
+            this.getGlobalCoinsData()
+        }
+        catch(err) {
+            this.setState({
+                isLoading: false,
+                error: err
+            })
+        }
+    }
+
+    handleChange = (e) => {
+        this.props.handleCurrencyChange(e.target.value)
+    }
+
     render() {
         return(
             <StyledNavbar>
                 <StyledLink to="/">CoinList</StyledLink>
                 <StyledLink to="/coinpage">Coinpage</StyledLink>
                 <StyledLink to="/portfolio">Portfolio</StyledLink>
+                <select onChange={this.handleChange}>
+                    <option value={"btc"}>BTC</option>
+                    <option value={"eth"}>ETH</option>
+                    <option value={"eur"}>EUR</option>
+                    <option value={"gbp"}>GBP</option>
+                    <option value={"usd"}>USD</option>
+                </select>
+                <NavbarGlobal currency={this.props.currency} data={this.state.globalData}/>
             </StyledNavbar>
         )
     }
---
+++ src/components/NavbarGlobal/NavbarGlobal.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledNavbarGlobal = styled.div`
+    background: black;
+    color: white;
+`
