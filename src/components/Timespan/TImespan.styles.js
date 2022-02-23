fd5081324ebb79e1382eeae62bba7a9d7356ebfc
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-02-23 02:53:34 -0600

    style timespan container

--- src/components/Timespan/TImespan.styles.js
+++ src/components/Timespan/TImespan.styles.js
@@ -2,9 +2,13 @@ import styled from "styled-components";
 
 export const StyledTimespan = styled.div`
     display: flex;
-    margin: 25px 192px 0px 192px;
+    margin: 0 auto;
+    width: 50%;
     padding: 15px 6px;
     justify-content: space-evenly;
+    background: grey;
+    border-radius: 10px;
+    color: white;
     div {
         background: blue;
         padding: 4px 14px;
@@ -16,5 +20,8 @@ export const StyledTimespan = styled.div`
     div:hover {
         background: green;
         cursor: pointer;
+    }
+    div:focus {
+        color: red;
     }
 `
