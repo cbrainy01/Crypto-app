5a5ad0b5aeb2fe9e8ff205f06f74b8f9bf37dba8
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-02-28 14:46:25 -0600

    render coin data on coinpage

--- src/pages/CoinPage/CoinPage.js
+++ src/pages/CoinPage/CoinPage.js
@@ -1,39 +1,240 @@
 import axios from "axios";
 import React from "react";
 import { ErrorDisplay } from "components";
+import { renderPercentChange, getCurrencySymbol, formatNumber } from "utils";
+import { CoinDescription, CoinLinks, CoinLink, CoinSummary, StyledCoinPage, SummaryA, SummaryB, SummaryC, Bar } from "./CoinPage.styles";
+import Link from "icons/Link.svg"
+import Stack from "icons/Stack.svg"
+import Uptick from "icons/Uptick.svg"
+import Downtick from "icons/Downtick.svg"
+import Plus from "icons/Plus.svg"
+import Copy from "icons/Copy.svg"
 
 export default class CoinPage extends React.Component {
-  state = { 
+  state = {
     isLoading: false,
     error: null,
-    coinData: null, 
+    coinData: null,
   };
 
   getCoinData = async () => {
-    this.setState({ isLoading: true })  
+    this.setState({ isLoading: true });
     try {
-        const {data} = await axios(`https://api.coingecko.com/api/v3/coins/${this.props.match.params.coin}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`)    
-        this.setState({ isLoading: false, coinData: data })
-    }catch(err) {
-        this.setState({ isLoading: false, error: err})
+      const { data } = await axios(
+        `https://api.coingecko.com/api/v3/coins/${this.props.match.params.coin}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`
+      );
+      this.setState({ isLoading: false, coinData: data });
+    } catch (err) {
+      this.setState({ isLoading: false, error: err });
     }
-  }
+  };
 
   componentDidMount = () => {
-    this.getCoinData()
-  }
+    this.getCoinData();
+  };
 
   componentDidUpdate = (prevProps) => {
-    if(prevProps.currency !== this.props.currency) {
-        this.getCoinData()
+    if (prevProps.currency !== this.props.currency) {
+      this.getCoinData();
     }
-  }
+  };
+
+  handleClipboardCopy = (e) => {
+    const copiedText = e.target.getAttribute("data-value");
+    navigator.clipboard.writeText(copiedText);
+  };
 
   render() {
-    return <div>
-        Coin Page
-        {this.state.error && <ErrorDisplay/>}
-        <h1>{this.state.coinData?.name}</h1>
-    </div>;
+    const data = this.state.coinData;
+    const currency = this.props.currency;
+    const circulating = Math.round(
+      (data?.market_data.circulating_supply / data?.market_data.max_supply) *
+        100
+    );
+    const volume = Math.round(
+      (data?.market_data.total_volume["btc"] / data?.market_data.max_supply) *
+        100
+    );
+    return (
+      <div>
+        {this.state.isLoading && <h1>...Loading</h1>}
+        {this.state.error && <ErrorDisplay />}
+        {this.state.coinData && (
+          <StyledCoinPage>
+            <h1>Your Summary:</h1>
+            <CoinSummary>
+              <SummaryA>
+                <section>
+                  <img src={data.image.small} alt={data.name} />
+                  <div>
+                    {data.name}({data.symbol.toUpperCase()})
+                  </div>
+                  <div>
+                    <img src={Link} alt="link icon" />
+                    <a href={data.links.homepage[0]}>
+                      {data.links.homepage[0]}
+                    </a>
+                  </div>
+                </section>
+              </SummaryA>
+              <SummaryB>
+                <section>
+                  <div>
+                    <span>
+                      {getCurrencySymbol(currency)}
+                      {data.market_data.current_price[currency]}
+                    </span>
+                    <span>
+                      {renderPercentChange(
+                        data.market_data.price_change_percentage_24h
+                      )}
+                    </span>
+                  </div>
+                  <div>
+                    <img src={Stack} alt="stack icon" />
+                  </div>
+                  <div>
+                    <span>
+                      <img src={Uptick} alt="uptick" />
+                    </span>
+                    <span>
+                      <p>
+                        All Time High: {getCurrencySymbol(currency)}
+                        {data.market_data.ath[currency]}
+                      </p>
+                      <p>{data.market_data.ath_date[currency]}</p>
+                    </span>
+                  </div>
+                  <div>
+                    <span>
+                      <img src={Downtick} alt="downtick" />
+                    </span>
+                    <span>
+                      <p>
+                        All Time Low: {getCurrencySymbol(currency)}
+                        {data.market_data.atl[currency]}
+                      </p>
+                      <p>{data.market_data.atl_date[currency]}</p>
+                    </span>
+                  </div>
+                </section>
+              </SummaryB>
+              <SummaryC>
+                <section>
+                  <div>
+                    <img src={Plus} alt="plus" />
+                    <p>Market cap: </p>
+                    <p>
+                      {getCurrencySymbol(currency)}
+                      {formatNumber(data.market_data.market_cap[currency])}
+                    </p>
+                    <p>
+                      {renderPercentChange(
+                        data.market_data.market_cap.change_percentage
+                      )}
+                    </p>
+                  </div>
+                  <div>
+                    <img src={Plus} alt="plus" />
+                    <p>Fully Diluted Valuation: </p>
+                    <p>
+                      {getCurrencySymbol(currency)}
+                      {formatNumber(
+                        data.market_data.fully_diluted_valuation[currency]
+                      )}
+                    </p>
+                  </div>
+                  <div>
+                    <img src={Plus} alt="plus" />
+                    <p>Volume 24h: {getCurrencySymbol(currency)}</p>
+                  </div>
+                  <div>
+                    <img src={Plus} alt="plus" />
+                    <p>Total Volume</p>
+                    <p>
+                      {data.market_data.total_volume["btc"]}{" "}
+                      {data.symbol.toUpperCase()}
+                    </p>
+                  </div>
+                  <div>
+                    <img src={Plus} alt="plus" />
+                    <p>Circulating Supply</p>
+                    <p>
+                      {data.market_data.circulating_supply}{" "}
+                      {data.symbol.toUpperCase()}
+                    </p>
+                  </div>
+                  <div>
+                    <img src={Plus} alt="plus" />
+                    <p>Max Supply</p>
+                    <p>
+                      {data.market_data.max_supply} {data.symbol.toUpperCase()}
+                    </p>
+                  </div>
+                  <div>
+                    <Bar circulating={circulating} volume={volume}>
+                      <div></div>
+                      <div></div>
+                    </Bar>
+                  </div>
+                </section>
+              </SummaryC>
+            </CoinSummary>
+            <h1>Description:</h1>
+            <CoinDescription>
+              <div
+                dangerouslySetInnerHTML={{ __html: data.description.en }}
+              ></div>
+            </CoinDescription>
+            <CoinLinks>
+              <div>
+                <CoinLink>
+                  <div>
+                    <a href={data.links.blockchain_site[0]} target="_blank">
+                      <img src={Link} alt="link" />
+                    </a>
+                    <p>{data.links.blockchain_site[0]}</p>
+                    <img
+                      onClick={this.handleClipboardCopy}
+                      data-value={data.links.blockchain_site[0]}
+                      src={Copy}
+                      alt="copy"
+                    />
+                  </div>
+                </CoinLink>
+                <CoinLink>
+                  <div>
+                    <a href={data.links.blockchain_site[1]} target="_blank">
+                      <img src={Link} alt="link" />
+                    </a>
+                    <p>{data.links.blockchain_site[1]}</p>
+                    <img
+                      onClick={this.handleClipboardCopy}
+                      data-value={data.links.blockchain_site[1]}
+                      src={Copy}
+                      alt="copy"
+                    />
+                  </div>
+                </CoinLink>
+                <CoinLink>
+                  <div>
+                    <a href={data.links.blockchain_site[2]} target="_blank">
+                      <img src={Link} alt="link" />
+                    </a>
+                    <p>{data.links.blockchain_site[2]}</p>
+                    <img
+                      onClick={this.handleClipboardCopy}
+                      data-value={data.links.blockchain_site[2]}
+                      src={Copy}
+                      alt="copy"
+                    />
+                  </div>
+                </CoinLink>
+              </div>
+            </CoinLinks>
+          </StyledCoinPage>
+        )}
+      </div>
+    );
   }
 }
---
+++ src/pages/CoinPage/CoinPage.styles.js
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+
+export const StyledCoinPage = styled.div`
+  margin-top: 4.1%;
+  a:link {
+    text-decoration: none;
+    color: ${(props) => props.theme.color};
+  }
+  h1 {
+    margin-left: 13.4%;
+  }
+`;
+
+export const CoinSummary = styled.div`
+  margin-top: 2.6%;
+  display: flex;
+  margin-left: 13.4%;
+  margin-right: 12.3%;
+  margin-bottom: 2.6%;
+`;
+
+export const SummaryA = styled.div`
+  border-radius: 12px;
+  background: ${(props) => props.theme.main};
+  section {
+    padding: 18px 36px;
+  }
+  margin-right: 3.8%;
+`;
+
+export const SummaryB = styled.div`
+  border-radius: 12px;
+  background: ${(props) => props.theme.main};
+  section {
+    padding: 18px 36px;
+  }
+  margin-right: 4.4%;
+`;
+
+export const SummaryC = styled.div`
+  border-radius: 12px;
+  background: ${(props) => props.theme.main};
+  section {
+    padding: 18px 36px;
+  }
+  div {
+    display: flex;
+  }
+`;
+
+export const Bar = styled.div`
+  width: 49.2%;
+  border-radius: 2px;
+  height: 0.4%;
+  background: blue;
+  display: flex;
+  div:nth-child(1) {
+    border-radius: 2px;
+    background: green;
+    width: ${(props) => props.volume}%;
+    height: 15px;
+  }
+
+  div:nth-child(2) {
+    border-radius: 2px;
+    width: ${(props) => props.circulating}%;
+    height: 15px;
+    background: white;
+  }
+`;
+
+export const CoinDescription = styled.div`
+  margin-top: 2.6%;
+  margin-left: 13.5%;
+  margin-right: 12.3%;
+  background: ${(props) => props.theme.main};
+  border-radius: 12px;
+  div:nth-child(1) {
+    margin: 0 auto;
+  }
+  div {
+    padding: 18px 36px;
+  }
+  a:link {
+    color: blue;
+    background: ${(props) => props.theme.inner};
+  }
+`;
+
+export const CoinLinks = styled.div`
+  margin-top: 1.4%;
+  margin-left: 13.5%;
+  margin-right: 12.3%;
+  div {
+    display: flex;
+    justify-content: space-evenly;
+  }
+`;
+
+export const CoinLink = styled.div`
+  display: flex;
+  justify-content: center;
+  background: ${(props) => props.theme.main};
+  border-radius: 12px;
+  padding: 8px 18px;
+  font-size: 15px;
+  div {
+    height: 15px;
+  }
+  p {
+    margin-right: 4.5%;
+  }
+  a {
+    margin-left: 1.14%;
+    margin-right: 4.68%;
+  }
+  img {
+    margin-left: 1.14%;
+    margin-right: 4.68%;
+    cursor: pointer;
+  }
+`;
