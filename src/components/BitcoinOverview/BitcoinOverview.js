d913db76c607f863c4fd2edd7b235d786a0bd6f9
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-03-08 14:46:25 -0600

    convert Coins.js to hooks

--- src/components/BitcoinOverview/BitcoinOverview.js
+++ src/components/BitcoinOverview/BitcoinOverview.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { PriceChart, VolumeChart } from 'components';
-import { StyledBitcoinOverview } from './BitcoinOverview.styles';
+import React from "react";
+import { PriceChart, VolumeChart } from "components";
+import { StyledBitcoinOverview } from "./BitcoinOverview.styles";
 
 export function BitcoinOverview(props) {
-        return (
-            <StyledBitcoinOverview>
-                <PriceChart currency={props.currency.toString()} timeSpan={props.timeSpan} />
-                <VolumeChart currency={props.currency.toString()} timeSpan={props.timeSpan} />
-            </StyledBitcoinOverview>
-        )
+  return (
+    <StyledBitcoinOverview>
+      <PriceChart
+        currency={props.currency.toString()}
+        timeSpan={props.timeSpan}
+      />
+      <VolumeChart
+        currency={props.currency.toString()}
+        timeSpan={props.timeSpan}
+      />
+    </StyledBitcoinOverview>
+  );
 }
 
-export default BitcoinOverview
-
+export default BitcoinOverview;
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import InfiniteScroll from "react-infinite-scroll-component"
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import queryString from "query-string";
-import axios from 'axios'
-import { Coin } from "components"
-import { reduceSparkline } from 'utils';
-import topDownArrow from "icons/topDownArrow.svg"
-import ArrowDown from "icons/ArrowDown.svg"
-import ArrowLeft from "icons/ArrowLeft.svg"
-import ArrowRight from "icons/ArrowRight.svg"
-import TornadoSort from "icons/TornadoSort.svg"
+import axios from "axios";
+import { Coin } from "components";
+import { reduceSparkline } from "utils";
+import topDownArrow from "icons/topDownArrow.svg";
+import ArrowDown from "icons/ArrowDown.svg";
+import ArrowLeft from "icons/ArrowLeft.svg";
+import ArrowRight from "icons/ArrowRight.svg";
+import TornadoSort from "icons/TornadoSort.svg";
 import {
   StyledCoins,
   SortingContainer,
@@ -24,161 +24,146 @@ import {
   TR,
 } from "./Coins.styles";
 
+export function Coins(props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [coinsData, setCoinsData] = useState(null);
+  const [sortDirection, setSortDirection] = useState("desc");
+  const [sortType, setSortType] = useState("volume");
+  const [hasMore, setHasMore] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
-export class Coins extends React.Component {
-  state = {
-    isLoading: false,
-    error: null,
-    coinsData: null,
-    sortDirection: "desc",
-    sortType: "volume",
-    hasMore: true,
-    currentPage: 1,
-  };
-
-  sortCoins = () => {
-    const { sortType, coinsData } = this.state;
-    if (this.state.ascSort === "asc") {
+  const sortCoins = () => {
+    if (sortDirection === "asc") {
       return coinsData.sort((a, b) => a[sortType] - b[sortType]);
-    } else if (this.state.ascSort === "desc") {
+    } else if (sortDirection === "desc") {
       return coinsData.sort((a, b) => b[sortType] - a[sortType]);
     } else {
       return coinsData;
     }
   };
 
-  getCoins = async () => {
-    this.setState({ isLoading: true });
+  const getCoins = async () => {
+    setIsLoading(true);
     try {
       const { data } = await axios(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page=${this.state.currentPage}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page=${currentPage}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
       );
       const updatedData = reduceSparkline(data);
-      if (!this.state.coinsData) {
-        this.setState({
-          coinsData: updatedData,
-          currentPage: this.state.currentPage + 1,
-        });
+      if (!coinsData) {
+        setCoinsData(updatedData);
+        setCurrentPage(currentPage + 1);
+        setIsLoading(false);
       } else {
-        this.setState({
-          coinsData: this.state.coinsData.concat(updatedData),
-          currentPage: this.state.currentPage + 1,
-        });
+        setCoinsData(coinsData.concat(updatedData));
+        setCurrentPage(currentPage + 1);
+        setIsLoading(false);
       }
       return data;
     } catch (error) {
-      this.setState({ isLoading: false, error: error });
+      console.error(error);
+      setIsLoading(false);
+      setError(error);
     }
   };
 
-  componentDidMount() {
-    this.getCoins();
-  }
-  
-  componentDidUpdate(prevProps) {
-    if(this.props.currency !== prevProps.currency) {
-        this.getCoins()
-    }
-  }
+  useEffect(() => {
+    getCoins();
+  }, [props.currency]);
 
-  fetchMoreCoins = () => {
-    if (this.state.coinsData?.length > 30) {
-      this.setState({ hasMore: false });
+  const fetchMoreCoins = () => {
+    if (coinsData?.length > 30) {
+      setHasMore(false);
       return;
     }
 
     setTimeout(() => {
-      console.log("getting more coins");
-      const response = this.getCoins();
+      const response = getCoins();
     }, 500);
   };
 
-  handleSortChange = (e) => {
+  const handleSortChange = (e) => {
     const att = e.target.getAttribute("data-value");
-    this.setState({ sortType: att });
+    setSortType(att);
   };
 
-  render() {
-    return (
-      <StyledCoins>
-        <SortingContainer>
-          <SortControl>
-            <img src={topDownArrow} alt="ethereum" />
-            <RankingSort>Top/bot #</RankingSort>
-            <SortBy>By Volume</SortBy>
-            <img src={ArrowDown} alt="arrowdown" />
-          </SortControl>
-          <DisplayCountControl>
-            <div>Show: /select tag/</div>
-            <div>PAGE</div>
-            <img src={ArrowLeft} />
-            <div>/current page/</div>
-            <img src={ArrowRight} />
-          </DisplayCountControl>
-        </SortingContainer>
-        <CoinsContainer>
-          <StyledTable>
-            <TableHead>
-              <TR>
-                <TH>#</TH>
-                <TH>
-                  <span>Name</span>
-                  <img
-                    onClick={this.handleSortChange}
-                    src={TornadoSort}
-                    alt="sortbutton"
-                    data-value={"name"}
-                  />
-                </TH>
-                <TH>
-                  <span>Price</span>
-                  <img
-                    onClick={this.handleSortChange}
-                    src={TornadoSort}
-                    alt="sortbutton"
-                    data-value={"current_price"}
-                  />
-                </TH>
-                <TH>
-                  <span>1h</span> <img src={TornadoSort} alt="sortbutton" />
-                </TH>
-                <TH>
-                  <span>24h</span> <img src={TornadoSort} alt="sortbutton" />
-                </TH>
-                <TH>
-                  <span>7d</span> <img src={TornadoSort} alt="sortbutton" />
-                </TH>
-                <TH>24h Vol / Market Cap</TH>
-                <TH>Circulating /Total Sup</TH>
-                <TH>Last 7d</TH>
-              </TR>
-            </TableHead>
-            <TableBody>
-              <InfiniteScroll
-                dataLength={
-                  this.state.coinsData ? this.state.coinsData.length : 10
-                }
-                next={this.fetchMoreCoins}
-                hasMore={this.state.hasMore}
-                loader={<h3>Loading...</h3>}
-                endMessage={<p>Thats all Folks!</p>}
-              >
-                {this.state.coinsData?.map((coinData, index) => (
-                  <Coin
-                    key={`${coinData.id} +${Math.random(20)}`}
-                    {...this.props}
-                    coinData={coinData}
-                    index={index + 1}
-                    currency={this.props.currency}
-                  />
-                ))}
-              </InfiniteScroll>
-            </TableBody>
-          </StyledTable>
-        </CoinsContainer>
-      </StyledCoins>
-    );
-  }
+  return (
+    <StyledCoins>
+      <SortingContainer>
+        <SortControl>
+          <img src={topDownArrow} alt="ethereum" />
+          <RankingSort>Top/bot #</RankingSort>
+          <SortBy>By Volume</SortBy>
+          <img src={ArrowDown} alt="arrowdown" />
+        </SortControl>
+        <DisplayCountControl>
+          <div>Show: /select tag/</div>
+          <div>PAGE</div>
+          <img src={ArrowLeft} />
+          <div>/current page/</div>
+          <img src={ArrowRight} />
+        </DisplayCountControl>
+      </SortingContainer>
+      <CoinsContainer>
+        <StyledTable>
+          <TableHead>
+            <TR>
+              <TH>#</TH>
+              <TH>
+                <span>Name</span>
+                <img
+                  onClick={handleSortChange}
+                  src={TornadoSort}
+                  alt="sortbutton"
+                  data-value={"name"}
+                />
+              </TH>
+              <TH>
+                <span>Price</span>
+                <img
+                  onClick={handleSortChange}
+                  src={TornadoSort}
+                  alt="sortbutton"
+                  data-value={"current_price"}
+                />
+              </TH>
+              <TH>
+                <span>1h</span> <img src={TornadoSort} alt="sortbutton" />
+              </TH>
+              <TH>
+                <span>24h</span> <img src={TornadoSort} alt="sortbutton" />
+              </TH>
+              <TH>
+                <span>7d</span> <img src={TornadoSort} alt="sortbutton" />
+              </TH>
+              <TH>24h Vol / Market Cap</TH>
+              <TH>Circulating /Total Sup</TH>
+              <TH>Last 7d</TH>
+            </TR>
+          </TableHead>
+          <TableBody>
+            <InfiniteScroll
+              dataLength={coinsData ? coinsData.length : 10}
+              next={fetchMoreCoins}
+              hasMore={hasMore}
+              loader={<h3>Loading...</h3>}
+              endMessage={<p>Thats all Folks!</p>}
+            >
+              {coinsData?.map((coinData, index) => (
+                <Coin
+                  key={`${coinData.id} +${Math.random(20)}`}
+                  {...props}
+                  coinData={coinData}
+                  index={index + 1}
+                  currency={props.currency}
+                />
+              ))}
+            </InfiniteScroll>
+          </TableBody>
+        </StyledTable>
+      </CoinsContainer>
+    </StyledCoins>
+  );
 }
 
-export default Coins
+export default Coins;
