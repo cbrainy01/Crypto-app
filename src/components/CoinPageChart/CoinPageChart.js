5767809ced57c85ca02b9ba896e5b19d45845424
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-03-08 19:47:00 -0600

    convert CoinPageChart.js to hooks

--- src/components/CoinPageChart/CoinPageChart.js
+++ src/components/CoinPageChart/CoinPageChart.js
@@ -1,74 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Line } from "react-chartjs-2";
-import { StyledCoinPageChart } from './CoinPageChart.styles'
+import { StyledCoinPageChart } from "./CoinPageChart.styles";
 
-export class CoinPageChart extends React.Component {
-  state = {
-    timespan: 24,
+export function CoinPageChart(props) {
+  const { data } = props;
+
+  const [timespan, setTimespan] = useState(24);
+
+  const lineData = data.slice(-timespan);
+  const labels = [...Array(lineData.length + 1).keys()].slice(1);
+
+  const handleTimespanChange = (e) => {
+    setTimespan(e.target.value);
   };
 
-  handleTimespanChange = (e) => {
-    this.setState({ timespan: e.target.value });
-  };
-
-  render() {
-    const lineData = this.props.data.slice(-this.state.timespan);
-    const labels = [...Array(lineData.length + 1).keys()].slice(1);
-    return (
-      <StyledCoinPageChart>
-        CoinPageChart
-        <select onChange={this.handleTimespanChange}>
-          <option value={12}>12h</option>
-          <option value={24}>1d</option>
-          <option value={72}>3d</option>
-          <option value={120}>5d</option>
-          <option value={168}>7d</option>
-        </select>
-        <Line
-          data={{
-            labels: labels,
-            datasets: [
-              {
-                data: lineData,
-                borderColor: "grey",
-              },
-            ],
-          }}
-          options={{
-            elements: {
-              point: { radius: 0 },
+  return (
+    <StyledCoinPageChart>
+      CoinPageChart
+      <select onChange={handleTimespanChange}>
+        <option value={12}>12h</option>
+        <option value={24}>1d</option>
+        <option value={72}>3d</option>
+        <option value={120}>5d</option>
+        <option value={168}>7d</option>
+      </select>
+      <Line
+        data={{
+          labels: labels,
+          datasets: [
+            {
+              data: lineData,
+              borderColor: "grey",
             },
-            scales: {
-              y: {
+          ],
+        }}
+        options={{
+          elements: {
+            point: { radius: 0 },
+          },
+          scales: {
+            y: {
+              display: false,
+              grid: {
                 display: false,
-                grid: {
-                  display: false,
-                },
-                ticks: {
-                  callback: function (value, index, ticks) {
-                    return;
-                  },
-                },
               },
-              x: {
-                grid: {
-                  display: false,
-                },
-                ticks: {
-                  callback: function (val, index) {
-                    return;
-                  },
+              ticks: {
+                callback: function (value, index, ticks) {
+                  return;
                 },
               },
             },
-            plugins: {
-              legend: { display: false },
+            x: {
+              grid: {
+                display: false,
+              },
+              ticks: {
+                callback: function (val, index) {
+                  return;
+                },
+              },
             },
-          }}
-        />
-      </StyledCoinPageChart>
-    );
-  }
+          },
+          plugins: {
+            legend: { display: false },
+          },
+        }}
+      />
+    </StyledCoinPageChart>
+  );
 }
 
-export default CoinPageChart
+export default CoinPageChart;
