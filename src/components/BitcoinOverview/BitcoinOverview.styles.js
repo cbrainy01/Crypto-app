9f89bff76fec83ae38d6e2fbda292dde984ce470
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-03-22 13:43:29 -0500

    center charts

--- src/components/BitcoinOverview/BitcoinOverview.styles.js
+++ src/components/BitcoinOverview/BitcoinOverview.styles.js
@@ -2,22 +2,24 @@ import styled from "styled-components";
 
 export const StyledBitcoinOverview = styled.div`
   margin-top: 12px;
+  margin: 0 auto;
 `;
 
 export const YourOverview = styled.p`
-  padding-left: 46px;
+  padding-left: 66px;
   margin-bottom: 15px;
+  font-weight: 600;
 `;
 
 export const ChartContainer = styled.div`
-  padding-left: 46px;
-  padding-right: 47px;
-  margin-bottom: 15px;
+  margin: 0 auto;
 `;
 
 export const ChartWrap = styled.div`
   width: 100%;
   display: flex;
+  justify-content: center;
+  margin: 0 auto;
 `;
 
 export const LoaderContainer = styled.div`
