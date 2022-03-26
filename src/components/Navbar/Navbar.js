ba77c6d9c82fa4ec0618c84133739c958d53a456
Carl-Brain Egebe <carlbrainegebe@gmail.com>
2022-03-26 12:09:58 -0500

    remove commented code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,40 +25,18 @@ import { toggleTheme } from "store/isBlacked/actions";
 import { getCurrency } from "store/currency/currencyReducer";
 
 export default function Navbar(props) {
-// function Navbar(props) {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [globalData, setGlobalData] = useState(null);
 
-  // const getGlobalCoinsData = async () => {
-  //   setLoading(true);
-
-  //   const response = await axios("https://api.coingecko.com/api/v3/global");
-  //   setLoading(false);
-  //   setGlobalData(response.data.data);
-  // };
-
-  // useEffect(() => {
-  //   try {
-  //     getGlobalCoinsData();
-  //   } catch (err) {
-  //     setLoading(false);
-  //     setError(err);
-  //   }
-  // }, []);
   const isBlacked = useSelector((state) => state.isBlacked)
   const currency = useSelector((state) => getCurrency(state))
   const dispatch = useDispatch()
-  // const handleChange = (e) => {
-  //   props.handleCurrencyChange(e.target.value);
-  // };
 
   return (
     <NavbarContainer>
       <StyledNavbar>
         <LeftNavbar>
-        {/* <button onClick={console.log("supp")}>click me</button> */}
-
           <LinkContainer text="coins">
             <StyledLink to={"/"}>Coins</StyledLink>
           </LinkContainer>
@@ -74,8 +52,6 @@ export default function Navbar(props) {
           <DropdownContainer>
             <span><p>{getCurrencySymbol(currency)}</p></span>
             <CurrencyDropdown>
-              {/* <select value={props.currency} onChange={handleChange}> */}
-              {/* <select value={props.currency} onChange={(e) => props.changeCurrency(e.target.value)}> */}
               <select value={currency} onChange={(e) => dispatch(changeCurrency(e.target.value))}>
                 <option value={"usd"}>USD</option>
                 <option value={"btc"}>BTC</option>
@@ -85,26 +61,13 @@ export default function Navbar(props) {
               </select>
             </CurrencyDropdown>
           </DropdownContainer>
-          {/* <ThemeToggleContainer onClick={() => props.handleThemeChange()}> */}
-          {/* <ThemeToggleContainer onClick={() => console.log("boppity boozz")  }> */}
           <ThemeToggleContainer onClick={() =>  dispatch(toggleTheme(!isBlacked)) }>
             <img src={Theme} alt="theme change icon" />
           </ThemeToggleContainer>
         </RightNavbar>
       </StyledNavbar>
-      {/* <NavbarGlobal currency={props.currency} globalData={globalData} /> */}
       <NavbarGlobal currency={props.currency} />
     </NavbarContainer>
   );
 }
 
-
-// const mapStateToProps = (state) => ({
-//   currency: getCurrency(state),
-// });
-
-// const mapDispatchToProps = {
-//   changeCurrency,
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
